0d8a40884cd4862a7d3ada9104ed1d3452039fbb
Vorontsov1 <evgeniyvorontsov1@gmail.com>
2023-01-02 13:10:26 +0200

    Profile_done

---
+++ src/components/App.js
@@ -0,0 +1,33 @@
+import { Profile } from 'components/Profile/Profile';
+// import {FriendList} from 'components/FriendList/FriendList';
+// import {FriendListItem} from 'components/FriendListItem/FriendListItem';
+// import {Statistics} from 'components/Statistics/Statistics';
+// import {TransactionHistory} from 'components/TransactionHistory/TransactionHistory';
+
+import profile from '../components/Data/profile.json';
+// import friends from '../components/Data/friends.json';
+// import statistics from '../components/Data/statistics.json';
+// import transaction from '../components/Data/transactions.json';
+
+
+
+
+
+export const App = () => {
+  return (
+    <div>
+      <Profile 
+      username={profile.username}
+      tag={profile.tag}
+      location={profile.location}
+      avatar={profile.avatar}
+      stats={profile.stats} />
+
+      {/* <FriendList />
+      <Statistics />
+      <TransactionHistory />  */}
+    </div>
+    
+   
+  );
+};
