d70a8818de7c1ac80a811c5eb50f7943f5741a7b
Vorontsov1 <evgeniyvorontsov1@gmail.com>
2023-01-02 17:40:31 +0200

    finished

--- src/components/Statistics/Statistics.js
+++ src/components/Statistics/Statistics.js
@@ -27,7 +27,7 @@ export const Statistics = ({ title, stats }) => {
     title: PropTypes.string,
     stats: PropTypes.arrayOf(
       PropTypes.shape({
-        id: PropTypes.number.isRequired,
+        id: PropTypes.string.isRequired,
         label: PropTypes.string.isRequired,
         percentage: PropTypes.number.isRequired,
       })
